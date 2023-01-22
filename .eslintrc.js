47eef124a034f3ab479779ce6c1c7af5aaee15ec
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2023-01-22 17:11:40 +0330

    feat: config typescript eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaVersion: 12,
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false,
-    sourceType: 'module',
-  },
   plugins: ['prettier'],
+  env: {
+    node: true,
+    es2021: true,
+    browser: true,
+  },
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
 }
